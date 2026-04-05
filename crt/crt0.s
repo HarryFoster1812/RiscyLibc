8188c3907c0232ceb5917fc2ec6c3e512faf9647
r49769hf <harry.foster-2@student.manchester.ac.uk>
2026-04-06 00:03:13 +0100

    added gitignore

--- crt/crt0.s
+++ crt/crt0.s
@@ -2,13 +2,13 @@
 
 .extern __libc_start_main
 .extern main
+.extern __exit
 .global _start
 
 _start:
 		# set up arguments (init argc and argv)
-		la main
+		la a0, main
     call __libc_start_main
 		
-		li a7, SYSCALL_EXIT
-		ecall
+		call __exit
 
